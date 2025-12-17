60d703d59fdda9ea74ed57740b28e5f31dce6117
MarcDevGuy127 <marceloh.github@gmail.com>
2025-12-17 10:37:01 -0300

    feat: add if else for hello world example.

--- src/hello_world/Main.kt
+++ src/hello_world/Main.kt
@@ -1,5 +1,16 @@
 package hello_world
 
 fun main() {
+    var x = 10
     print("Hello World!") //print Hello World
+
+   if(x < 10) {
+       print("\n x is minor than 10")
+   }
+    if(x == 10) {
+        print("\n x is equal 10")
+    }
+    if(x > 10) {
+        print("\n x is higher than 10")
+    }
 }
